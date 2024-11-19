1ce4e65aebf0c64cc61c5d7228825d1bbd6d8013
yuwenshuai <yuwenshuai@autohome.com.cn>
2024-11-19 20:43:08 +0800

    feat: 项目搭建-vite&nextjs&tailwind&react&typescript

---
+++ src/pages/components/layout/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p className="h-10 bg-[#f90] text-[#0f0]">Footer content</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/components/layout/navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <nav className="h-20 bg-[#fff] text-[#09f]">Navbar content</nav>;
+};
+
+export default Navbar;
